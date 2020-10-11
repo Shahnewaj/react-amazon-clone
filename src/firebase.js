1e3fec7f85af0662212ac16b01b63e42e01e09eb
shahnewaj <shahnewajtxt@gmail.com>
2020-10-11 23:22:08 +0600

    React Firebase Amazon Clone

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBFfLhpxxrBJt1hTPwIenQ-23NbBVUYA3A",
+    authDomain: "helloclone.firebaseapp.com",
+    databaseURL: "https://helloclone.firebaseio.com",
+    projectId: "helloclone",
+    storageBucket: "helloclone.appspot.com",
+    messagingSenderId: "968175882035",
+    appId: "1:968175882035:web:a9d9d72e18b787f4edb846",
+    measurementId: "G-8RP5R600VJ"
+  };
